645f5e6b17d75c3911cd5a0e8096bbacb7a82e89
JosephBrooke <joseph.brooke1@gmail.com>
2022-05-01 22:12:36 +0100

    fixed col data

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import tkinter as tk
 from tkinter import CENTER, LEFT, ttk
 from tkinter import messagebox
 from pymongo import MongoClient, collection
+import ui
 
-root = tk.Tk()
+root = ui.MainUI()
 root.title("Advanced Programming")
 
 # closing function
@@ -21,41 +22,8 @@ def hide_button(widget):
     # This will remove the widget from toplevel
     widget.pack_forget()
 
-
-# show all db results
-def query_collection_result(col_name="airports"):
-    # create connection with localhost
-    client = MongoClient("mongodb://localhost:27017/")
-    db = client["demo"]
-    coll: collection.Collection = db[col_name]
-    # turn columns into a dictionary
-    columns = list(coll.find_one({}).keys())
-    columns.remove("_id")
-    print(columns)
-    # render tree
-    # get current size of main window
-    curMainWindowHeight = root.winfo_height()
-    curMainWindowWidth = root.winfo_width()
-
-    tree = ttk.Treeview(root, columns=columns, show="headings")
-    for key in columns:
-        print(key)
-        tree.column(key, anchor=CENTER,)
-        tree.heading(f"{key}", text=str(key))
-
-    tree.grid(row=1, column=1, sticky=tk.EW)
-    # add a scrollbar
-    scrollbar_vert = ttk.Scrollbar(root, orient=tk.VERTICAL, command=tree.yview)
-    scrollbar_horz = ttk.Scrollbar(root, orient=tk.HORIZONTAL, command=tree.xview)
-    tree.configure(yscrollcommand=scrollbar_vert.set, xscrollcommand=scrollbar_horz)
-    scrollbar_vert.grid(row=1, column=2, sticky="ns")
-    scrollbar_horz.grid(row=2, column=1, sticky="we")
-
-    # In command show_button() function is passed to recover Button B1
-    back_button = tk.Button(root, text="Button 3", command=tree.state("disabled"))
-    back_button.grid(column=2, row=3, sticky=tk.SW, padx=1, pady=1)
-
-def create_main_window(window_width=1200, window_height = 600):
+# main tinker window layout
+def create_main_window(window_width=1200, window_height=600):
     # create tinker window with fixed size as in widthxheight±x±y
     window_width = 1200
     window_height = 600
@@ -71,8 +39,10 @@ def create_main_window(window_width=1200, window_height = 600):
     # set the position of the window to the center of the screen
     root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
 
+
+# start the ui loop
 def start_window():
-     # fix blur in ui
+    # fix blur in ui
     try:
         from ctypes import windll
 
@@ -81,17 +51,13 @@ def start_window():
         root.protocol("WM_DELETE_WINDOW", on_closing)
         root.mainloop()
 
-if __name__ == "__main__":
-    
-    create_main_window()
 
-    # createa the container grid
-    root.columnconfigure(0, weight=3)
-    root.columnconfigure(1, weight=6)
-    root.columnconfigure(2, weight=6)
-    # place a label on the root window
-    message = tk.Label(root, text="Airport Data Analysis", font=("Times New Roman", 18))
-    message.grid(column=1, row=0, sticky=tk.EW, padx=5, pady=5)
+# function to create main window
+def create_button_frame(container):
+
+    frame = ttk.Frame(container)
+
+    frame.columnconfigure(0, weight=1)
 
     # create a button to show airport data
     all_data = ttk.Button(
@@ -100,11 +66,15 @@ if __name__ == "__main__":
     all_data.grid(column=0, row=1, sticky=tk.EW, padx=1, pady=1)
 
     # frequency data button
-    freq_data = ttk.Button(root, text="show Frequency Data", command=query_collection_result)
+    freq_data = ttk.Button(
+        root, text="show Frequency Data", command=query_collection_result
+    )
     freq_data.grid(column=0, row=2, sticky=tk.EW, padx=1, pady=1)
 
     # runway data button
-    runway_data = ttk.Button(root, text="Show Runway Data", command=query_collection_result)
+    runway_data = ttk.Button(
+        root, text="Show Runway Data", command=query_collection_result
+    )
     runway_data.grid(column=0, row=3, sticky=tk.EW, padx=1, pady=1)
 
     # uk data
@@ -117,10 +87,23 @@ if __name__ == "__main__":
     quit_button = ttk.Button(root, text="Quit", command=on_closing)
     quit_button.grid(column=0, row=5, sticky=tk.EW, padx=1, pady=1)
 
+    for widget in frame.winfo_children():
+        widget.grid(padx=0, pady=3)
+
+    return frame
+
+
+if __name__ == "__main__":
+
+    create_main_window()
+
+    # createa the container grid
+    root.columnconfigure(0, weight=3)
+    root.columnconfigure(1, weight=6)
+    root.columnconfigure(2, weight=6)
+
     # create event binding, press esc to exit
     root.bind("<Escape>", lambda e: close_win(e, root=root))
 
     start_window()
-   
-
 
--- ui.py
+++ ui.py
@@ -1,2 +1,130 @@
-def button_clicked():
-    print('Button clicked')
+from dataclasses import dataclass
+import tkinter as tk
+from tkinter import CENTER, LEFT, ttk
+from pymongo import MongoClient, collection, database
+
+LARGE_FONT = ("Verdana", 12)
+
+
+class PageWithDB(tk.Frame):
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["demo"]
+    collection_name: collection.Collection
+
+    def __init__(self, parent):
+        super().__init__(self, parent)
+
+    def get_col_names(self):
+        coll: collection.Collection = self.db[self.collection_name]
+        # turn columns into a dictionary
+        columns = list(coll.find_one({}).keys())
+        columns.remove("_id")
+        return columns
+
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Welcome to Airport Data", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button = tk.Button(
+            self,
+            text="All Airport Data",
+            command=lambda: controller.show_frame(AirportPage),
+        )
+        button.pack()
+
+        button2 = tk.Button(
+            self, text="UK Airport Data", command=lambda: controller.show_frame(PageTwo)
+        )
+        button2.pack()
+
+
+class AirportPage(PageWithDB):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        # return to home page
+        button1 = tk.Button(
+            self, text="Back to Home", command=lambda: controller.show_frame(StartPage)
+        )
+        button1.pack()
+        label = tk.Label(self, text="All airport data", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        self.collection_name = "airports"
+
+        # render all columns
+        columns = self.get_col_names()
+
+        # add horizontal and vertical scrollbar
+        # scrollbar
+        scroll_bar = tk.Scrollbar(self)
+        scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        scroll_bar = tk.Scrollbar(self, orient="horizontal")
+        scroll_bar.pack(side=tk.BOTTOM, fill=tk.X)
+
+        tree = ttk.Treeview(
+            self,
+            columns=columns,
+            show="headings",
+            xscrollcommand=scroll_bar.set,
+            yscrollcommand=scroll_bar.set,
+        )
+        for key in columns:
+            print(key)
+            tree.column(
+                key, anchor=CENTER,
+            )
+            tree.heading(f"{key}", text=str(key))
+        
+        # pack the data and configuer the view
+        tree.pack()
+        scroll_bar.config(command=tree.yview)
+        scroll_bar.config(command=tree.xview)
+
+
+class PageTwo(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(
+            self, text="Back to Home", command=lambda: controller.show_frame(StartPage)
+        )
+        button1.pack()
+
+        button2 = tk.Button(
+            self, text="Page One", command=lambda: controller.show_frame(AirportPage)
+        )
+        button2.pack()
+
+
+class MainUI(tk.Tk):
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, AirportPage, PageTwo):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
