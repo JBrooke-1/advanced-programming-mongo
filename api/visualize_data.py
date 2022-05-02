7f6c7a1a50fb395ecf6052178a4b97f6d3229061
JosephBrooke <joseph.brooke1@gmail.com>
2022-05-02 18:59:46 +0100

    refactored draw function

--- api/visualize_data.py
+++ api/visualize_data.py
@@ -45,6 +45,50 @@ def draw_small_airport_freq():
         title="communication frequencies used by uk's small_airports"
     )
 
+# draw big airport freq
+def draw_big_airport_freq():
+    collection_name="uk-airports-frequencies"
+    search_params={"type": "big_airport", "frequency_mhz": {"$gt" : 100}}
+
+    # find all big from the uk airport db
+    big_uk_airports = db.find_in_db(
+        collection_name=collection_name,
+        search_params=search_params,
+        fields_to_return={"airport_ref": 1, "frequency_mhz": 1, "name": 1, "_id": 0},
+    )
+    big_uk_airports_max = db.find_maxmin_val(
+        collection_name=collection_name,
+        field="frequency_mhz",
+        search_params=search_params,
+        fields_to_return={"frequency_mhz": 1, "_id": 0},
+    )
+
+    big_uk_airports_min = db.find_maxmin_val(
+        collection_name=collection_name,
+        is_ascending=True,
+        field="frequency_mhz",
+        search_params=search_params,
+        fields_to_return={"frequency_mhz": 1, "_id": 0},
+    )
+    print(
+        "max herz is", big_uk_airports_max, " and min herz is ", big_uk_airports_min
+    )
+
+    average = db.find_small_airport_average(
+        collection_name="uk-airports-frequencies",
+        search_params={"type": "small_airport"},
+    )
+    print(average)
+
+    # draw the final result
+    draw_result(
+        big_uk_airports,
+        min_val=big_uk_airports_min,
+        max_val=big_uk_airports_max,
+        average=average,
+        title="communication frequencies used by uk's small_airports"
+    )
+
 
 def draw_result(query_result, min_val, max_val, average, title):
 
@@ -75,6 +119,6 @@ def draw_result(query_result, min_val, max_val, average, title):
     plt.ylabel("Numbers of counts")
     plt.show()
 
-
+draw_big_airport_freq()
 draw_small_airport_freq()
 
